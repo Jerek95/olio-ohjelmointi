14aabe32f75ddd3148d28715f7daef383dd5e205
Jere Koivuranta <jere.koivuranta@gmail.com>
2025-01-08 17:00:21 +0200

    Ensimmainen viikkotehtava tehty.

--- teht1/main.cpp
+++ teht1/main.cpp
@@ -1,9 +1,53 @@
 #include <iostream>
-
+#include <cstdlib>
 using namespace std;
+
+int game(int maxnum);
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int lukujenMaara;
+    int tulos;
+
+    cout << "Syota kuinka monen luvun joukosta haluat arvata" << endl;
+    cin >> lukujenMaara;
+    tulos = game(lukujenMaara);
+    cout << "Arvausten maara oli "<< tulos << endl;
+
     return 0;
 }
+
+
+
+int game(int maxnum){
+    int arvottuLuku;
+    int pelaajanArvaus;
+    int arvaustenMaara = 0;
+    int peliKaynnissa = 1;
+
+    srand(13);
+    arvottuLuku = rand() % maxnum + 1;
+
+    while (peliKaynnissa == 1){
+        cout << "Syota arvaus" << endl;
+        cin >> pelaajanArvaus;
+
+        if (pelaajanArvaus < arvottuLuku){
+            cout << "Liian pieni" << endl;
+        }
+        else if (pelaajanArvaus > arvottuLuku){
+            cout << "Liian suuri" << endl;
+        }
+        else if (pelaajanArvaus == arvottuLuku){
+            cout << "Oikein" << endl;
+            peliKaynnissa = 0;
+        }
+        arvaustenMaara++;
+    }
+
+    return arvaustenMaara;
+}
+
+
+
+
